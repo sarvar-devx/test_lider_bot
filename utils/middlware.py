79f36336259a380e0889079c4deac08e86fefc5c
sarvar-py-dev <sarvarbekdavranbekov@gmail.com>
2024-10-28 10:29:41 +0500

    change channels

--- utils/middlware.py
+++ utils/middlware.py
@@ -2,7 +2,7 @@ from typing import Iterable
 
 from aiogram import BaseMiddleware, Bot
 from aiogram.enums import ChatMemberStatus
-from aiogram.types import InlineKeyboardButton
+from aiogram.types import InlineKeyboardButton, Message
 from aiogram.types.update import Update
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
@@ -22,19 +22,22 @@ async def make_channels_button(channel_ids: Iterable, bot: Bot):
 
 class SubscriptionMiddleware(BaseMiddleware):
     async def __call__(self, handler, update: Update, data):
+        message = update.event
+        if type(message) is not Message:
+            message = message.message
         unsubscribe_channels = [channel_id for channel_id in conf.bot.CHANNELS if (
             await update.bot.get_chat_member(channel_id, update.event.from_user.id)).status == ChatMemberStatus.LEFT]
         if unsubscribe_channels:
             ikb = await make_channels_button(unsubscribe_channels, update.bot)
             ikb.row(InlineKeyboardButton(text='✅ Tasdiqlash',
-                                         callback_data='channel_confirm__' + str(update.event.message_id)))
+                                         callback_data='channel_confirm__' + str(message.message_id)))
             await update.event.answer("Botni foydalanishdan oldin kannalarga a'zo bo'ling",
                                       reply_markup=ikb.as_markup())
             return
 
         user = await User.get(update.event.from_user.id)
         if not user and not await data['state'].get_state():
-            await send_first_name(update.event, data['state'])
+            await send_first_name(message, data['state'])
             return
 
         return await handler(update, data)
